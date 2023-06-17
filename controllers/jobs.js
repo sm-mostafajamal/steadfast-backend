faa513abce26ea7324cf37de992b7ee9da659341
sm-mostafajamal <sm.mostafajamal@gmail.com>
2023-06-17 07:17:31 +0600

    added jwt backend

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
 const Job = require("../models/job");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 router.get("/", async (req, res, next) => {
   const jobs = await Job.find({});
@@ -23,9 +33,14 @@ router.get("/:id", async (req, res, next) => {
 router.post("/", async (req, res, next) => {
   try {
     const content = req.body;
-
+    const decodeToken = jwt.verify(getTokenFrom(req), process.env.SECRET);
+    if (!decodeToken.id) {
+      return res.status(401).json({ error: "Invalid Token" });
+    }
+    const user = await User.findById(decodeToken.id);
     const job = new Job({
       ...content,
+      user: user._id,
     });
 
     const savedJob = await job.save();
--- utils/middleware.js
+++ utils/middleware.js
@@ -4,6 +4,10 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
     return res.status(400).send({ error: "content is missing" });
+  } else if (error.name === "JsonWebTokenError") {
+    return res.status(400).send({ error: error.message });
+  } else if (error.name == +"TokenExpiredError") {
+    return res.status(400).json({ error: "Token expired" });
   }
 
   next(error);
